f66de52d227708b6610add1b11415267f1efd1a7
sjorv <sjorvensa_@outlook.com>
2022-03-30 23:08:26 -0400

    made it a GRU instead of a RNN

--- rnn.py
+++ rnn.py
@@ -5,7 +5,7 @@ from helper import *
 import hyper_param
 from tqdm import tqdm
 
-HIDDEN_SIZE = 10
+HIDDEN_SIZE = 20
 
 WEIGHTS = torch.tensor([0.01, 1, 1, 2, 2])
 
@@ -27,7 +27,7 @@ class taikoRNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.hidden_size = HIDDEN_SIZE
-        self.rnn = nn.RNN(input_size=hyper_param.n_mels, hidden_size=self.hidden_size)
+        self.rnn = nn.GRU(input_size=hyper_param.n_mels, hidden_size=self.hidden_size)
         self.fc = nn.Linear(self.hidden_size, 5)
     
     def forward(self, x):
@@ -60,6 +60,8 @@ def train_rnn_network(model, num_iters=100, learning_rate=1e-3, wd=0, checkpoint
     
         if checkpoint_path and iter_num % 100 == 0:
             torch.save(model.state_dict(), checkpoint_path.format(iter_num))
+    
+    return losses
 
 model = taikoRNN()
-train_rnn_network(model, learning_rate = 1e-4, num_iters=10000, wd=0, checkpoint_path="checkpoint/iter-{}.pt")
+train_rnn_network(model, learning_rate = 4e-4, num_iters=10000, wd=0, checkpoint_path="checkpoint/iter-{}.pt")
