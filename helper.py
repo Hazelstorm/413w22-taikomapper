b6043741fe70cdb23a3124aaff53944eedab8606
sjorv <sjorvensa_@outlook.com>
2022-04-12 21:30:31 -0400

    changed description of get_npy_data

--- helper.py
+++ helper.py
@@ -79,10 +79,7 @@ def ms_to_snap(bar_len, offset, ms, snap_val=SNAP):
         return round(snap_num)
 
 """
-Returns numpy data stored by create_data() in datasets.py. 
-
-If get_timing is False (default), returns audio_data and notes_data only
-Otherwise, returns audio_data, notes_data, timing
+Returns numpy data (audio_data, notes_data, timing) stored by create_data() in datasets.py.
 """
 def get_npy_data(path):
     notes_data = np.load(os.path.join(path, "notes_data.npy"))
