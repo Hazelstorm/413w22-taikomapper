86805837ec42ba687bd80880cf81ddb0fcfdf10b
sjorv <sjorvensa_@outlook.com>
2022-04-14 01:24:44 -0400

    made audio smoother

--- preprocessing_helpers.py
+++ preprocessing_helpers.py
@@ -131,15 +131,16 @@ def get_map_audio(filepath, convert=True, kwargs=get_mel_param()):
 
     # TODO Uh taken from github... will change when I understand how to use librosa 
 
-    D = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length,
-                            win_length=win_length)) ** pwr_spect
+    # D = np.abs(librosa.stft(y, n_fft=n_fft, hop_length=hop_length,
+    #                         win_length=win_length)) ** pwr_spect
 
-    S = librosa.feature.melspectrogram(S=D, n_fft=n_fft, n_mels=n_mels,
-                                       fmin=fmin, fmax=fmax)
+    # S = librosa.feature.melspectrogram(S=D, n_fft=n_fft, n_mels=n_mels,
+    #                                    fmin=fmin, fmax=fmax)
 
-    dbs = librosa.core.power_to_db(S)
+    S = librosa.feature.melspectrogram(y, n_fft=n_fft, n_mels=n_mels, hop_length=hop_length,
+                                        win_length=win_length, fmin=fmin, fmax=fmax)
 
-    spectro = np.squeeze(dbs).T
+    spectro = np.squeeze(S).T
 
     return spectro
         
