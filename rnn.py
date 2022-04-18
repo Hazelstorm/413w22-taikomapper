36c7249fc5583766f7a2114da1c9e577740344b5
sjorv <sjorvensa_@outlook.com>
2022-04-17 20:03:57 -0400

    documented some lines

--- rnn.py
+++ rnn.py
@@ -16,7 +16,7 @@ class notePresenceRNN(nn.Module):
         self.fc = nn.Linear(self.hidden_size * 2, 1)
     
     def forward(self, audio_windows):
-        audio_windows = torch.unsqueeze(audio_windows, dim=0)
+        audio_windows = torch.unsqueeze(audio_windows, dim=0)  # Batch size is always 1
         out = self.embedding(audio_windows)
         out, _ = self.rnn(out)
         out = self.fc(out)
@@ -35,17 +35,21 @@ class noteColourRNN(nn.Module):
         self.fc = nn.Linear(self.hidden_size * 2, 1)
     
     def forward(self, audio_windows, notes_data):
+        # Concatenate notes_data to the end of audio_windows
         notes_data = torch.transpose(notes_data, 0, 1)
         augmented_spectro = torch.cat((audio_windows, notes_data), 1)
         augmented_spectro = torch.unsqueeze(augmented_spectro, dim=0)
+        
         out = self.embedding(augmented_spectro)
         out, _ = self.rnn(out)
         out = self.fc(out)
         out = torch.squeeze(out, dim=2)
         out = torch.squeeze(out, dim=0)
+
         # Allow the model to only colour notes that are present in notes_data
         notes_present = notes_data > 0
         out = torch.mul(out, torch.squeeze(notes_present, dim=1))
+        
         return out
 
 class noteFinisherRNN(noteColourRNN):
